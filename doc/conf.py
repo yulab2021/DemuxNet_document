381552f8aab7de2d4925f8d00f47ef7f37c5087f
Wu You <wuyou@localhost.localdomain>
2026-02-08 22:14:21 +0800

    Update theme

--- doc/conf.py
+++ doc/conf.py
@@ -83,8 +83,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
-#html_theme = 'classic'    # 经典的浅色主题
+#html_theme = 'sphinx_rtd_theme'
+html_theme = 'classic'    # 经典的浅色主题
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
