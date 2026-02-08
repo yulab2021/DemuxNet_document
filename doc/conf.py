772083e6586a2d06b9a2fc0ebeb95498053856e6
Wu You <wuyou@localhost.localdomain>
2026-02-08 22:17:42 +0800

    Update theme

--- doc/conf.py
+++ doc/conf.py
@@ -85,6 +85,8 @@ todo_include_todos = False
 #
 #html_theme = 'sphinx_rtd_theme'
 html_theme = 'classic'    # 经典的浅色主题
+html_theme = "alabaster"
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
